db1b7eb70e09755769e0704318ad98f94af5f5b8
ModestaLiatuke <94903955+ModestaLiatuke@users.noreply.github.com>
2022-03-29 16:05:25 +0300

    TodoList

---
+++ backend/index.js
@@ -0,0 +1,236 @@
+import express from 'express'
+import cors from 'cors'
+import { readFile, writeFile } from 'fs'
+
+
+const app = express()
+const database = 'database.json'
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(express.urlencoded({
+    extended: false
+}))
+
+
+
+app.get('/', (req, res) => {
+
+
+    readFile(database, 'utf8', (err, data) => {
+        if (err) {
+            res.json({ status: "failed", message: "Nepavyko perskaityti failo" })
+
+        } else {
+            data = JSON.parse(data)
+            res.json({ status: 'success', data })
+
+        }
+    })
+})
+
+app.get('/:id', (req, res) => {
+    let id = req.params.id
+
+    readFile(database, 'utf8', (err, data) => {
+        if (err) {
+            res.json({ status: 'failed', message: 'Nepavyko perskaityti failo' })
+        } else {
+            data = JSON.parse(data)
+
+            const jsonId = data.findIndex((el) => el.id == id)
+
+            if (jsonId === -1) {
+                res.json({ status: 'failed', message: 'Nepavyko rasti tokio elemento' })
+                return
+            }
+
+            res.json({ status: 'success', data: data[jsonId] })
+        }
+    })
+
+})
+
+app.post('/add-todo', (req, res) => {
+    let task = req.body.task
+
+    readFile(database, 'utf8', (err, data) => {
+        if (err) {
+            res.json({ status: "failed", message: "Nepavyko perskaityti failo" })
+            return
+        }
+
+        let json = JSON.parse(data)
+        let id = json.length > 0 ? json[json.length - 1].id + 1 : 0;
+
+        //Alternatyva auksciau aprasytai eilutei
+        // if (json.length > 0)
+        //     id = json[json.length - 1].id + 1
+
+        json.push({ id, task, done: false })
+
+        writeFile(database, JSON.stringify(json), 'utf8', err => {
+            if (err) {
+                res.json({ status: "failed", message: "Nepavyko perskaityti failo" })
+            } else {
+                res.json({ status: "success", message: "Duomenys sėkmingai įrašyti" })
+
+            }
+        })
+    })
+})
+
+app.delete('/delete-todo/:id', (req, res) => {
+    let id = req.params.id
+
+    readFile(database, 'utf8', (err, data) => {
+        if (err) {
+            res.json({ status: "failed", message: "Nepavyko perskaityti failo" })
+            return
+        }
+        //issifruojame Json informacija atgal i javascript masyva
+        let json = JSON.parse(data)
+
+        const jsonId = json.findIndex((el) => el.id == id)
+
+        if (jsonId === -1) {
+            res.json({ status: "failed", message: "Nepavyko rasti tokio elemento" })
+            return
+        }
+        json.splice(jsonId, 1)
+
+        let jsonString = JSON.stringify(json)
+
+        writeFile(database, jsonString, 'utf8', (err) => {
+            if (err) {
+                res.json({ status: "failed", message: "Nepavyko perskaityti failo" })
+
+            } else {
+                res.json({ status: "success", message: "Įrašas sėkmingai ištrintas" })
+
+            }
+        })
+
+    })
+
+})
+
+app.delete('/mass-delete', (req, res) => {
+    let ids = req.body.ids
+
+    readFile(database, 'utf8', (err, data) => {
+        if (err) {
+            res.json({ status: "failed", message: "Nepavyko perskaityti failo" })
+            return
+        }
+        //issifruojame Json informacija atgal i javascript masyva
+        let json = JSON.parse(data)
+        let dataArray = []
+
+
+        json.forEach((value, index) => {
+            if (!ids.includes(value.id.toString())) {
+                dataArray.push(value)
+            }
+
+        })
+
+        let jsonString = JSON.stringify(dataArray)
+
+        writeFile(database, jsonString, 'utf8', (err) => {
+            if (err) {
+                res.json({ status: "failed", message: "Nepavyko perskaityti failo" })
+
+            } else {
+                res.json({ status: "success", message: "Įrašas sėkmingai ištrintas" })
+
+            }
+        })
+
+    })
+
+})
+
+app.put('/mark-done/:id', (req, res) => {
+    let id = req.params.id
+
+    readFile(database, 'utf8', (err, data) => {
+        if (err) {
+            res.json({ status: "failed", message: "Nepavyko perskaityti failo" })
+            return
+        }
+        //issifruojame Json informacija atgal i javascript masyva
+        let json = JSON.parse(data)
+
+        const jsonId = json.findIndex((el) => el.id == id)
+        if (jsonId === -1) {
+            res.json({ status: "failed", message: "Nepavyko rasti tokio elemento" })
+            return
+        }
+        //atstatymo eilute
+        json[jsonId].done = json[jsonId].done ? false : true
+
+        let jsonString = JSON.stringify(json)
+
+        writeFile(database, jsonString, 'utf8', (err) => {
+            if (err) {
+                res.json({ status: "failed", message: "Nepavyko perskaityti failo" })
+
+            } else {
+                res.json({ status: "success", message: "Užduotis atlikta" })
+            }
+        })
+    })
+})
+
+app.put('/edit-todo/:id', (req, res) => {
+    let id = req.params.id
+    let task = req.body.task
+
+    if (task === undefined) {
+        res.json({ status: "failed", message: "Neįvesta tekstas" })
+        return
+    }
+
+
+    readFile(database, 'utf8', (err, data) => {
+        if (err) {
+            res.json({ status: "failed", message: "Nepavyko perskaityti failo" })
+            return
+        }
+        //issifruojame Json informacija atgal i javascript masyva
+        let json = JSON.parse(data)
+
+        const jsonId = json.findIndex((el) => el.id == id)
+        if (jsonId === -1) {
+            res.json({ status: "failed", message: "Nepavyko rasti tokio elemento" })
+            return
+        }
+
+        json[jsonId].task = task
+
+        let jsonString = JSON.stringify(json)
+
+        writeFile(database, jsonString, 'utf8', (err) => {
+            if (err) {
+                res.json({ status: "failed", message: "Nepavyko perskaityti failo" })
+
+            } else {
+                res.json({ status: "success", message: "Įrašas paredaguotas" })
+            }
+        })
+    })
+})
+
+
+
+
+
+
+
+
+app.listen(3001, () => {
+    console.log('Serveris veikia')
+})
